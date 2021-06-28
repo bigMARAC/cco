b2b6338b46c862b4186456d3a6118622a2fb9700
bigMARAC <marcos.length@gmail.com>
2021-06-28 14:45:41 -0300

    resolve 1959

--- INE5402-01208B/operadores-aritmeticos/1959.py
+++ INE5402-01208B/operadores-aritmeticos/1959.py
@@ -1,2 +1,7 @@
-index, length = map(int, input().split(' '))
-print(length * index)
+import math
+first = list(map(float, input().split(' ')))
+second = list(map(float, input().split(' ')))
+
+x = ((second[0] - first[0]) ** 2) + ((second[1] - first[1]) ** 2)
+x = math.sqrt(x)
+print("%.4f" % x)
