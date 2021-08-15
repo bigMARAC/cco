6d7172ad3ee3ce28c3a0e8cae392de3878d41c20
bigMARAC <marcos.length@gmail.com>
2021-08-14 23:08:06 -0300

    add comments to 2803

--- INE5402-01208B/prova-02/2803.py
+++ INE5402-01208B/prova-02/2803.py
@@ -1,6 +1,9 @@
 statesList = ["acre", "amapa", "amazonas", "para", "rondonia", "roraima", "tocantins"]
 state = input()
 
+# Pra esse problema basta ter uma lista com todos 
+# os estados da região norte e checar e o valor
+# recebido no input está nessa lista
 if state in statesList:
   print("Regiao Norte")
 else:
