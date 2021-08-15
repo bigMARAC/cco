cb4a1663a46d5a690fe9d9cfbd39be837b377cec
bigMARAC <marcos.length@gmail.com>
2021-08-14 23:00:54 -0300

    add comments to 2322

--- INE5402-01208B/prova-02/2322.py
+++ INE5402-01208B/prova-02/2322.py
@@ -2,10 +2,15 @@ x = int(input())
 values = list(map(int, input().split(' ')))
 values = sorted(values)
 
+# Depois de ordenar a lista, basta fazer um simples IF
+# pra saber que peça está faltando
 for y in range(0, len(values)):
   if values[y] != (y + 1):
     break
 
+# Se no primeiro loop não foi encontrada a peça que falta
+# Então provavelmente a peça que falta é a última peça
+# Ex.: 1 2 3, a peça que falta é a 4
 if values[y] == (y + 1):
   y += 1
 
