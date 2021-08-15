5c3462b4b711ad2ab62d39d65ad40bda92ffcc5c
bigMARAC <marcos.length@gmail.com>
2021-08-14 23:18:58 -0300

    add comments to 3117

--- INE5402-01208B/prova-02/3117.py
+++ INE5402-01208B/prova-02/3117.py
@@ -2,6 +2,14 @@ length, minimum = map(int, input().split(' '))
 data = list(map(int, input().split(' ')))
 
 count = 0
+
+# Aqui, fazemos um loop em todos os tempos
+# da lista 'data', se algum desses tempos for
+# menor ou igual a 0, significa que esse aluno
+# não se atrasou, então incrementamos a variável
+# auxiliar. No final, checamos se a quantidade de
+# alunos que não se atrasaram, é maior ou igual
+# a quantidade de alunos minima informada pelo professor
 for item in data:
   if item <= 0:
     count += 1
