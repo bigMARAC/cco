cbd7176676f02e4184bee1acc4e65a5a3b0c153f
bigMARAC <marcos.length@gmail.com>
2021-08-14 23:14:13 -0300

    add comments to 3059

--- INE5402-01208B/prova-02/3059.py
+++ INE5402-01208B/prova-02/3059.py
@@ -2,6 +2,13 @@ length, minimum, maximum = map(int, input().split(' '))
 numbers = list(map(int, input().split(' ')))
 
 count = 0
+
+# Basta fazer um loop X nos números e depois um loop Y
+# e dentro do loop Y, podemos checar se a soma do número
+# do loop X com o número do loop Y está de acordo com
+# as regras recebidas por input, se sim, incrementamos a 
+# váriavel auxiliar. No final precisamos dividir por dois
+# visto que o IF será aceito em duas ocasiões iguais
 for num in numbers:
   for num_aux in numbers:
     if num != num_aux and num + num_aux <= maximum and num + num_aux >= minimum:
