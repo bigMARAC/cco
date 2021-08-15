4b6877ff0142335a5855d42c07d6ee2ffd563213
bigMARAC <marcos.length@gmail.com>
2021-08-14 23:06:40 -0300

    add comments to 2473

--- INE5402-01208B/prova-02/2473.py
+++ INE5402-01208B/prova-02/2473.py
@@ -2,6 +2,11 @@ values = list(map(int, input().split(' ')))
 winning = list(map(int, input().split(' ')))
 count = 0
 
+# Esse problema é bastante simples, basta fazer um loop
+# nos números apostados e dentro desse, fazer um loop nos
+# números certos, depois é só comporar o número X com os N
+# números certos, e incrementar o 'count' toda vez que algum
+# número for igual
 for item in values:
   for number in winning:
     if item == number:
