f22bb6d6ef17a26dd032dba2918b9d7612b272e7
bigMARAC <marcos.length@gmail.com>
2021-08-14 23:11:07 -0300

    add comments to 3039

--- INE5402-01208B/prova-02/3039.py
+++ INE5402-01208B/prova-02/3039.py
@@ -1,5 +1,9 @@
 index = int(input())
 girls, boys = 0, 0
+
+# Nesse problema, pegamos o segundo valor do input
+# que informa o sexo da criança e incrementamos
+# a váriavel de acordo com o valor recebido
 for x in range (0, index):
   gift_type = list(input().split(' '))[1]
   if gift_type == 'M':
