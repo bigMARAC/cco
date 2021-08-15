4a16afc4333b7c9d30bc34c08593c45a8064d5b4
bigMARAC <marcos.length@gmail.com>
2021-08-14 23:09:36 -0300

    add comments to 3038

--- INE5402-01208B/prova-02/3038.py
+++ INE5402-01208B/prova-02/3038.py
@@ -2,6 +2,10 @@ while True:
   try:
     text = input()
     if text == "": break;
+
+    # Basta usar a função replace no texto
+    # substituindo todos os caracteres oferecidos
+    # pelo problema
     text = text.replace("@", "a")
     text = text.replace("&", "e")
     text = text.replace("!", "i")
