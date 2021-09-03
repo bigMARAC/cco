4c46faa278fe31f48d2f3c04803e32ecc4354586
bigMARAC <marcos.length@gmail.com>
2021-09-03 17:33:31 -0300

    resolve 2450

--- INE5402-01208B/prova-03/2450.py
+++ INE5402-01208B/prova-03/2450.py
@@ -4,42 +4,30 @@ matriz = []
 for x in range(rows):
   matriz.append(list(map(int, input().split())))
 
-zeroCols, zeroLine = [], -1
-anyTime, valid = False, False
-for x in range(rows):
-  aux = True
+valid = True
+all_zeros = False
+qtd_zeros_ultima_linha = -1
 
-  for y in range(cols):
-    
-    # Aqui é checado se a coluna atual precisa ser uma coluna de zeros
-    if y in zeroCols:
-      # Se precisar, é checado se o número atual e zero e se essa regra
-      # já foi quebrada alguma vez
-      if matriz[x][y] == 0 and not anyTime:
-        valid = True
-      else:
-        valid = False
-        anyTime = True
-        break
-    
-    # Aqui é checado se o número atual é diferente de zero e 
-    # se o número a sua esquerda é um zero, se isso for verdadeiro
-    # então todos os próximos números dessa coluna precisarão ser zero
-    if matriz[x][y] != 0 or y == 0:
-      if y == 0 or matriz[x][y - 1] == 0:
-        zeroCols.append(y)
+for i in range(rows):
+  j = 0
+  try:
+    while matriz[i][j] == 0:
+      j += 1
+  except IndexError:
+    all_zeros = True
+
+  if not all_zeros:
+    if j <= qtd_zeros_ultima_linha:
+      valid = False
+      break
     else:
-      aux = False
+      qtd_zeros_ultima_linha = j
+  else:
+    if j != cols:
+      valid = False
+      break
 
-    if zeroLine >= 0 and zeroLine == x - 1:
-      if matriz[x][y] != 0:
-        valid = False
-        break
-  
-  if aux:
-    zeroLine = x
-
-if (valid):
+if valid:
   print('S')
 else:
   print('N')
